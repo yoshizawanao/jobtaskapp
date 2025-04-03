a81f738db07b2576657f513c7971127950e8d329
naoyoshi323 <205914113+yoshizawanao@users.noreply.github.com>
2025-04-03 13:33:48 +0900

    [feat]文字追加

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+
+def init_page():
+    st.set_page_config(
+        page_title="Ask My PDF(s)",
+        page_icon="🧐"
+    )
+
+
+def main():
+    init_page()
+
+    st.sidebar.success("👆のメニューから進んでね")
+
+    st.markdown(
+    """
+    ### Ask My PDF(s) にようこそ！
+
+    - このアプリでは、アップロードしたPDFに対して質問をすることができます。
+    - まずは左のメニューから `📄 Upload PDF(s)` を選択してPDFをアップロードしてください。
+    - PDFをアップロードしたら `🧐 PDF QA` を選択して質問をしてみましょう😇
+    """
+    "Ask My PDF(s) にようこそ！
+
+    - このアプリでは、アップロードしたPDFに対して質問をすることができます。
+    - まずは左のメニューから `📄 Upload PDF(s)` を選択してPDFをアップロードしてください。
+    - PDFをアップロードしたら `🧐 PDF QA` を選択して質問をしてみましょう😇"
+    )
+
+if __name__ == '__main__':
+    main()
